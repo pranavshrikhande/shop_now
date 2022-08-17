0b7b3cbdbc3266e466253c3d8f718c4d7edd02c2
pranavshrikhande <pranav.shrikhande@gmail.com>
2022-08-17 13:13:36 -0400

    frontend

--- backend/shopnow/src/main/java/com/code/shopnow/dao/ProductCategoryRepository.java
+++ backend/shopnow/src/main/java/com/code/shopnow/dao/ProductCategoryRepository.java
@@ -3,10 +3,12 @@ package com.code.shopnow.dao;
 import com.code.shopnow.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 /*
  * productCategory------------>JSON ENTRY
  * 
  * */
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel="productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
 
--- backend/shopnow/src/main/java/com/code/shopnow/dao/ProductRepository.java
+++ backend/shopnow/src/main/java/com/code/shopnow/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.code.shopnow.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.code.shopnow.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	
